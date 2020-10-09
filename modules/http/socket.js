9155989f0959426b2b132410bfb30fc731b8d1a1
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2020-10-09 20:50:03 +0200

    Full update

--- modules/http/socket.js
+++ modules/http/socket.js
@@ -8,16 +8,35 @@ var fs = require('fs');
 var app = express();
 
 console.info('\x1b[36m%s\x1b[0m', 'Initialisation du WebSocket sur le port '+config.webport+'..');
+
 var http = require('https');
 var options = {
   key: fs.readFileSync(config.sslkey),
   cert: fs.readFileSync(config.sslcertificate)
 };
+
 var socket = http.createServer(options, app).listen(config.webport);
 
 var io = require('socket.io')(socket);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
+
+var userNeedToWait = [];
+
+async function addUserNeedToWait(user) {
+  userNeedToWait.push(user);
+}
+
+async function removeUserNeedToWait(user) {
+  var index = userNeedToWait.indexOf(user);
+  if(index > -1) {
+    userNeedToWait.splice(index, 1);
+  }
+}
+
+async function userIsOnListToWait(user) {
+  return userNeedToWait.includes(user);
+}
 
 app.get('/', function (req, res, next){
   res.redirect(config.url);
@@ -31,15 +50,26 @@ io.sockets.on('connection', async function(s) {
 
     if(auth_response == true) {
 
-      socket.emit('auth', 'success')
+      if(await userIsOnListToWait(token_key)) {
+        socket.emit('auth', 'needToWait')
+      } else {
+        socket.emit('auth', 'success')
+      }
 
       socket.on('stepOne', async (invite) => {
 
         if(!invite) return socket.emit('newError', "Please specify an invitation !");
         invite = invite.toString().replace(/https:\/\/|http:\/\/|discord\.gg\/|discord\.com\//gi, '');
+	//console.log(invite.toString.replace(/discord(?:(?:app)?\.com\/invite|\.gg(?:\/invite)?)\/([\w-]{2,255})/i, '');
 
         var stepOne = await cloner.stepOne(invite, token_key);
         if(stepOne[0] == false) return socket.emit('newError', stepOne[1]);
+
+        await addUserNeedToWait(token_key);
+
+        setTimeout(async function() {
+          await removeUserNeedToWait(token_key);
+        }, 600000);
 
         socket.emit('stepTwo', stepOne[2]);
       });
